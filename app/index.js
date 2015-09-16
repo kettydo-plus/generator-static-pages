a15ed4a8173fe2791c5bec28d717490ba8f2dded
mgobbi <marcogobbi0@gmail.com>
2015-09-16 17:05:05 +0200

    creazione generator

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 //var chalk = require('chalk');
 
-var OnepageGenerator = yeoman.generators.Base.extend({
+var OnepagchalkeGenerator = yeoman.generators.Base.extend({
     promptUser: function() {
         var done = this.async();
 
