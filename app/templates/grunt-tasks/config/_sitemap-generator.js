4721e74ee8efca849821333d9073f13597ae8b5d
mgobbi <marcogobbi0@gmail.com>
2015-09-17 16:13:01 +0200

    "sitemap-generator"

--- app/templates/grunt-tasks/config/_sitemap-generator.js
+++ app/templates/grunt-tasks/config/_sitemap-generator.js
@@ -1,20 +1,14 @@
 /**
  * Created by mgobbi on 02/09/2015.
  */
-var utils=require("../utils/Utils");
+var utils = require("../utils/Utils");
 module.exports = {
     // crea gli xlsx dai json
     "htdocs": {
 
-        files: [{
-            expand: true,
-            cwd: utils.getS,
-            src: [
-                '*.html',
-                "?*-tpl.html"
-            ],
-            dest: utils.getSourcePath(utils.JSON)+"pages",
-            ext: '.json'
-        }]
+        files: {
+            "src/sitemap.html": "src",
+            "htdocs/sitemap.html": "src"
+        }
     }
 };
--- app/templates/grunt-tasks/tasks/_sitemap-generator.js
+++ app/templates/grunt-tasks/tasks/_sitemap-generator.js
@@ -1,25 +1,48 @@
-
 var fs = require("fs");
+var R = require("ramda");
 module.exports = function (grunt) {
 
-    grunt.registerTask("sitemap-generator",function(){
+    function makeGetPromise(templateStart, templateEnd) {
 
-        var template =fs.readFileSync("grunt-tasks/utils/html-template.html", "utf-8");
-        console.log(template);
+        var makeGetTemplate = function (writeFile) {
+            return R.compose(
+                writeFile,
+                data=> data.concat("</ul>" + templateEnd),
+                R.reduce((prev, curr)=>  prev + "<li><a href=\"" + curr + "\" >" + curr + "</a></li>", templateStart + "<ul>"),
+                R.filter(file=> (file.match(/.html/g) && !file.match(/-tpl.html/g)))
+            );
+        };
+        var writeFile = R.curryN(4, (url, resolve, reject, sitemapData)=> {
 
-        var files = fs.readdirSync("src");
+            fs.writeFile(url, sitemapData, 'utf8', err=> {
+                if (err) {
+                    reject();
+                } else {
+                    resolve(sitemapData);
+                }
 
-         var content= files.filter(function(file) { return file.substr(-5) === '.html'; })
-            .reduce(function(prev,next) {
-                return prev + "<li href=\""+next+"\" >"+next+"</li>";
-            },"");
+            });
+        });
 
-         template = template.replace("{{content}}",content);
-         console.log(template);
+        return (dirname, dest)=>  new Promise((resolve, reject)=> {
+            fs.readdir(dirname, R.flip(makeGetTemplate(writeFile(dest, resolve, reject))));
+        });
 
-         fs.writeFile("grunt-tasks/utils/sitemap.html", template, 'utf8', function (err) {
-                 if (err) return console.log(err);
-         });
+    }
+
+    grunt.registerMultiTask("sitemap-generator", function () {
+
+        var done = this.async();
+        var getPromise = makeGetPromise("<html><head></head><body>", "</body></html>");
+
+        var getSitemap = R.compose(
+            R.bind(Promise.all, Promise),
+            R.flatten,
+            R.map(f=>R.map(dirname=>getPromise(dirname, f.dest), f.src))
+        );
+        getSitemap(this.files)
+            .then(console.log.bind(console, "sitemap done"))
+            .then(done)
 
     });
 };
